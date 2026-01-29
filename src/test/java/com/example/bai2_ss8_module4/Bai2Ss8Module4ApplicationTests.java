3df00e37134cafaa76b3561f1a5f2e999a1d442c
tiger2131231 <duongvuthai2018@gmail.com>
2026-01-29 19:20:56 +0700

    update ss8 bai2

---
+++ src/test/java/com/example/bai2_ss8_module4/Bai2Ss8Module4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bai2_ss8_module4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai2Ss8Module4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
